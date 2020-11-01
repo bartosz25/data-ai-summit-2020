9fa9ea403f9b0c42773db555a8bbfba36bc4b2f1
bartosz25 <bartkonieczny@yahoo.fr>
2020-11-01 10:50:36 +0100

    clarify the input dataset

--- demos/src/main/scala/com/waitingforcode/data/configuration/DropDuplicatesDataGeneratorConfiguration.scala
+++ demos/src/main/scala/com/waitingforcode/data/configuration/DropDuplicatesDataGeneratorConfiguration.scala
@@ -9,6 +9,8 @@ object DropDuplicatesDataGeneratorConfiguration extends DataGenerationConfigurat
     Seq("""{"event_time": "2020-05-05T03:22:00", "id": 2, "value": 1}"""),
     // Same id (dropDuplicates uses it) but different event_time, won't be integrated
     Seq("""{"event_time": "2020-05-05T01:25:00", "id": 1, "value": 1}"""),
-    Seq("""{"event_time": "2020-05-05T01:22:00", "id": 1, "value": 1}""")
+    Seq("""{"event_time": "2020-05-05T01:22:00", "id": 1, "value": 1}"""),
+    // New id but too old to be integrated (watermark)
+    Seq("""{"event_time": "2020-05-05T01:22:00", "id": 3, "value": 1}""")
   )
 }
