dd3fa308001a488475d1b915a9319c8faeb601dd
bartosz25 <bartkonieczny@yahoo.fr>
2020-11-01 11:02:22 +0100

    fix state store error due to double read of the same file in the same micro-batch

--- demos/src/main/scala/com/waitingforcode/statestore/MapDBStateStoreProvider.scala
+++ demos/src/main/scala/com/waitingforcode/statestore/MapDBStateStoreProvider.scala
@@ -7,8 +7,6 @@ import org.apache.spark.sql.execution.streaming.state.{StateStore, StateStoreCon
 import org.apache.spark.sql.types.StructType
 import org.mapdb.{DBMaker, Serializer}
 
-import scala.collection.JavaConverters.asScalaSetConverter
-
 class MapDBStateStoreProvider extends StateStoreProvider with Logging {
 
   private var keySchema: StructType = _
@@ -18,6 +16,9 @@ class MapDBStateStoreProvider extends StateStoreProvider with Logging {
   private var lastCommittedVersion = NoCommittedVersionFlag
   private var houseKeeper: MapDBStateStoreHouseKeeper = _
   private var namingFactory: MapDBStateStoreNamingFactory = _
+
+
+  private var previousStateStoreInstance: MapDBStateStore = null
 
   private lazy val db = DBMaker
     .fileDB(namingFactory.allEntriesFile)
@@ -71,18 +72,30 @@ class MapDBStateStoreProvider extends StateStoreProvider with Logging {
         .restoreFromSnapshot()
         .applyUpdatesAndDeletes()
         .getAllEntriesMap
-      restoredEntries.getEntries.asScala.foreach(entry => println(entry))
       mapWithAllEntries.putAll(restoredEntries)
       logInfo(s"State restored correctly! Got ${mapWithAllEntries.size()} entries")
     }
 
+    // Most of the time, the state store is read linearly, eg. one version by a micro-batch execution
+    // *BUT* some of the operations like aggregations involve 2 readings. The first happens in
+    // StateStoreRestoreExec where the previously set aggregation value is retrieved. The second happens
+    // in StateStoreSaveExec.
+    // MapDB, unless checksumHeaderBypass() and fileLockDisable() methods are called in DBMaker,
+    // doesn't accept 2 processes reading the same database file. Moreover, disabling header checksum could
+    // lead to some data corruption.
+    // That's the reason why we always keep the last state instance. It shouldn't be problematic for the
+    // memory pressure because both local maps (updates and deletes) are cleaned at commit() or abort() call.
+    if (previousStateStoreInstance == null || lastCommittedVersion != version ) {
+      val stateStoreToReturn = new MapDBStateStore(previousVersion = version, mapAllEntriesDb = db,
+        mapWithAllEntries = mapWithAllEntries,
+        namingFactory = namingFactory,
+        performLocalSnapshot = (version + 1 % this.stateStoreConf.minDeltasForSnapshot == 0),
+        id = stateStoreIdValue,
+        keySchema = this.keySchema, valueSchema = this.valueSchema)
+      previousStateStoreInstance = stateStoreToReturn
+    }
     lastCommittedVersion = version
-    new MapDBStateStore(previousVersion = version, mapAllEntriesDb = db,
-      mapWithAllEntries = mapWithAllEntries,
-      namingFactory = namingFactory,
-      performLocalSnapshot = (version + 1 % this.stateStoreConf.minDeltasForSnapshot == 0),
-      id = stateStoreIdValue,
-      keySchema = this.keySchema, valueSchema = this.valueSchema)
+    previousStateStoreInstance
   }
 
   override def doMaintenance(): Unit = {
