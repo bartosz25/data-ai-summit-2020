57f84bb41bd01c06a96ac85af948e1f7e5b7f71c
bartosz25 <bartkonieczny@yahoo.fr>
2020-11-05 05:53:28 +0100

    rewrite the output if already exists

--- demos/src/main/scala/com/waitingforcode/stateful/BatchFilesWriter.scala
+++ demos/src/main/scala/com/waitingforcode/stateful/BatchFilesWriter.scala
@@ -1,10 +1,10 @@
 package com.waitingforcode.stateful
 
-import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.{Dataset, SaveMode}
 
 class BatchFilesWriter[T](inputPath: String) extends ((Dataset[T], Long) => Unit) {
   override def apply(datasetToWrite: Dataset[T], batchNumber: Long): Unit = {
-    datasetToWrite.write.json(s"${inputPath}/${batchNumber}")
+    datasetToWrite.write.mode(SaveMode.Overwrite).json(s"${inputPath}/${batchNumber}")
     println(s"Wrote ${batchNumber} batch")
   }
 }
